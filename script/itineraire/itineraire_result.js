9879b699d217a6a78ebdf35eedb60211473895b9
Louis <louisraverdy@gmail.com>
2023-06-30 14:27:10 +0200

    Ajout du tri par nombre de correspondances. Ajout en beta du détail d'un itinéraire. Correctifs de bugs.

--- script/itineraire/itineraire_result.js
+++ script/itineraire/itineraire_result.js
@@ -22,17 +22,20 @@ async function sortBy(sortType) {
     } else if (sortType == 1) {
         sorted_travels = itineraires_result
         sorted_travels.sort(function(a, b) {
-            return a.departTime - b.departTime
+            return a.previewArray.length - b.previewArray.length
         })
     } else if (sortType == 2) {
+        sorted_travels = itineraires_result
+        sorted_travels.sort(function(a, b) {
+            return a.departTime - b.departTime
+        })
+    } else if (sortType == 3) {
         sorted_travels = itineraires_result
         sorted_travels.sort(function(a, b) {
             return a.destinationTime - b.destinationTime
         })
     }
-    
-    showTravels()
-    return
+    await showTravels()
 }
 
 async function showTravels() {
@@ -73,29 +76,61 @@ async function showTravels() {
             }
         }
 
-        result_list.insertAdjacentHTML('beforeend', `<button class="result-child horizontal-container" onClick='resultClick()'>
+        result_list.insertAdjacentHTML('beforeend', `<div class="result-child vertical-container" id="result-${travel}">
+                                                        <button class="result-child-btn horizontal-container" id="${travel}" onClick='resultClick(this.id)'>
                                                             <div class="result-child-times vertical-container">
-                                                            <div class="result-child-time">${travel_info.departTime.getHours() < 10 ? "0" + travel_info.departTime.getHours() : travel_info.departTime.getHours()}h${travel_info.departTime.getMinutes() < 10 ? "0" + travel_info.departTime.getMinutes() : travel_info.departTime.getMinutes()}</div>
-                                                            <div class="result-child-time">${travel_info.destinationTime.getHours() < 10 ? "0" + travel_info.destinationTime.getHours() : travel_info.destinationTime.getHours()}h${travel_info.destinationTime.getMinutes() < 10 ? "0" + travel_info.destinationTime.getMinutes() : travel_info.destinationTime.getMinutes()}</div>
-                                                        </div>
-                                                        <div class="result-child-stations vertical-container">
-                                                            <div class="result-child-station">${travel_info.stationDepartName}</div>
-                                                            <div class="result-child-station">${travel_info.stationDestinationName}</div>
-                                                        </div>
-                                                        <div class="result-child-separator"></div>
-                                                        <div class="result-child-travel vertical-container">
-                                                            <div class="result-child-travel-preview horizontal-container">
-                                                                ${previewLignes}
+                                                                <div class="result-child-time">${travel_info.departTime.getHours() < 10 ? "0" + travel_info.departTime.getHours() : travel_info.departTime.getHours()}h${travel_info.departTime.getMinutes() < 10 ? "0" + travel_info.departTime.getMinutes() : travel_info.departTime.getMinutes()}</div>
+                                                                <div class="result-child-time">${travel_info.destinationTime.getHours() < 10 ? "0" + travel_info.destinationTime.getHours() : travel_info.destinationTime.getHours()}h${travel_info.destinationTime.getMinutes() < 10 ? "0" + travel_info.destinationTime.getMinutes() : travel_info.destinationTime.getMinutes()}</div>
                                                             </div>
-                                                            <div class="result-child-travel-time horizontal-container">
-                                                                <img class="result-child-travel-time-img" src="../../src/chrono.png">
-                                                                <div class="result-child-travel-time-text">${travel_time}</div>
+                                                            <div class="result-child-stations vertical-container">
+                                                                <div class="result-child-station">${travel_info.stationDepartName}</div>
+                                                                <div class="result-child-station">${travel_info.stationDestinationName}</div>
                                                             </div>
+                                                            <div class="result-child-separator"></div>
+                                                            <div class="result-child-travel vertical-container">
+                                                                <div class="result-child-travel-preview horizontal-container">
+                                                                    ${previewLignes}
+                                                                </div>
+                                                                <div class="result-child-travel-time horizontal-container">
+                                                                    <img class="result-child-travel-time-img" src="../../src/chrono.png">
+                                                                    <div class="result-child-travel-time-text">${travel_time}</div>
+                                                                </div>
+                                                            </div>
+                                                        </button>
+                                                        <div class="result-child-detail" id="details-${travel}">
                                                         </div>
-                                                    </button>`)
+                                                    </div>`)
     }
 }
 
-function resultClick() {
-    parent.showError("En développement", "Le detail du trajet est actuellement en cours de développement.")
+function resultClick(id) {
+    let button = document.getElementById(`result-${id}`).classList
+    let details = document.getElementById(`details-${id}`)
+    if (button.contains('expand')) {
+        button.remove('expand')
+        details.innerHTML = ''
+    } else {
+        button.add('expand')
+        for (i in sorted_travels[id].detailTravel) {
+            const detailData = sorted_travels[id].detailTravel[i]
+            var position = "mid";
+            var dotType = "-dot";
+    
+            if (i < 1) {
+                position = "top"
+            } else if (i == detailData.length - 1){
+                position = "bot"
+            }
+    
+            if (detailData.isTerminus == true) {
+                dotType = "-terminus"
+            }
+    
+            if (i == detailData.length - 1){
+                details.nsertAdjacentHTML('beforeend', `<div class="station-cell" onClick='changeStation(this.id)'><div class="station-cell-route"><div class="station-cell-route-rect-${position}" style="${ici}"></div><div class="station-cell${dotType}-mask"><div class="station-cell${dotType}"></div></div></div><div class="station-cell-container"><div class="station-cell-name${dotType}">${detailData.arret}</div></div></div>`)
+            } else {
+                details.insertAdjacentHTML('beforeend', `<div class="station-cell" onClick='changeStation(this.id)'><div class="station-cell-route"><div class="station-cell-route-rect-${position}"></div><div class="station-cell${dotType}-mask"><div class="station-cell${dotType}"></div></div></div><div class="station-cell-container"><div class="station-cell-name${dotType}">${detailData.arret}</div></div></div>`)
+            }
+        }
+    }
 }
